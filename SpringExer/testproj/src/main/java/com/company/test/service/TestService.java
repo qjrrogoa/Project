482194c2b58ee338ee4c70e98e6b00bd9da6c262
Daemin Park <qjrrogoa@naver.com>
2021-07-02 20:07:10 +0900

    프로젝트 연습

--- SpringExer/testproj/src/main/java/com/company/test/service/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.company.test.service;
-
-import java.util.Map;
-
-public interface TestService {
-
-	//회원가입
-	int insert(TestDTO dto);
-}
--- SpringExer/testproj/src/main/java/com/company/test/service/impl/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.company.test.service.impl;
